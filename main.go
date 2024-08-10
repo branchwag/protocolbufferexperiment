ea7b863ea4e162f2770559784fdff0f6d457b5f7
LeAnne B. <branchleannelmb@gmail.com>
2024-08-10 14:19:55 -0400

    accesses protobuff via main go file

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"main/model"
 
 	"github.com/golang/protobuf/proto"
 )
 
 func main () {
-	fmt.Println("Hai")
+	book := &model.Book{
+		Id: 1,
+		Title: "The Road to Wigan Pier",
+		Authors: []*model.Author{
+			{Id: 1, Name: "George Orwell"},
+		},
+		Category: model.Category_Novel,
+	}
+	
+	data, err := proto.Marshal(book)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(data)
 }
